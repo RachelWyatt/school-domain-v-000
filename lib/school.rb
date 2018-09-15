63cc518f5dc6c035d03b2f27ecf652f550229a5e
Rachel Wyatt <rw126@evansville.edu>
2018-09-15 14:59:45 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,8 +9,8 @@ class School
   end
 
   def add_student(grade, student)
-    roster["grade"] = []
-    roster["grade"] << "#{student}"
+    roster[grade] = []
+    roster[grade] << "#{student}"
   end
 
 end
